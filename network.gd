228546d16a6a00234d2501dd0194fc18c8fcfd6c
Hiếu Phạm Đình Trung <hieuphamdinhtrung@Hieus-MacBook-Air.local>
2023-12-06 11:23:52 +0700

    create admin and button mode, send and receive data from client to server

--- network.gd
+++ network.gd
@@ -12,6 +12,7 @@ var state = FREEZE
 
 func _ready():
 	multiplayer.connected_to_server.connect(self.joined)
+	multiplayer.server_disconnected.connect(self.disconnected)
 
 
 func _process(delta):
@@ -19,6 +20,10 @@ func _process(delta):
 
 func joined():
 	get_tree().change_scene_to_file("res://main.tscn")
+	
+
+func disconnected():
+	get_tree().change_scene_to_file("res://Lobby/Lobby.tscn")
 
 func change_scene(_state):
 	rpc("change_state", _state)
@@ -28,5 +33,14 @@ func change_state(_state):
 	print(str(username, ": ", _state))
 	state = _state
 	joined()
+	
+func send_data(data):
+	rpc_id(1,"recieve_data", data)
+
+@rpc("call_local", "any_peer")
+func recieve_data(data):
+	print(data)
+	if state == BUTTON:
+		pass
 
 
